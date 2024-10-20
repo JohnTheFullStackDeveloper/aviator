db86e24a372fdac004d54236e8751150ae9cfb47
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-10-20 23:36:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import  io from 'socket.io-client'
+const socket = io('https://check-t8r7.onrender.com')
+socket.on("getX",data=>{ 
+  if(data == "gone"){
+    document.getElementById("y").innerText = data
+}
+  else{
+  document.getElementById("y").innerText = ""
+  document.getElementById("x").innerText = data
+  }
+})
+function App() {
+  return (
+    <div className="App">
+      <h1 id='x'>hello bro</h1>
+      <h1 id='y'></h1>
+      <button id="bet">bet</button>
+      
+    </div>
+  );
+}
+
+export default App;
