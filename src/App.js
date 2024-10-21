733457c595d6d9f09dd2bdaefb9b70ff8f06f0a1
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-10-21 16:35:08 +0530

    superjesus

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
 import  io from 'socket.io-client'
+import { useState } from 'react';
+let isGameRunning =true;
+let PlayingBet = 0;
+let winning = 0;
+let isBet = false;
 const socket = io('https://check-t8r7.onrender.com')
+let messages;
 socket.on("getX",data=>{ 
   if(data == "gone"){
+    isGameRunning = false;
+    PlayingBet = 0
+    document.getElementById("bet").innerText = "Bet"
+    winning = 0
+    isBet = false
     document.getElementById("y").innerText = data
 }
   else{
+  isGameRunning = true;
+  if(isBet){
+    console.log(PlayingBet * data)
+    winning = PlayingBet * data
+    document.getElementById("bet").innerText = "cash out "+winning.toFixed(2)
+  }
   document.getElementById("y").innerText = ""
   document.getElementById("x").innerText = data
   }
 })
-let money = 10.11;
+function onBet(money,setMoney){
+  messages = document.getElementById("messages")
+  if(isBet){
+    setMoney(Number((money+winning).toFixed(2)))
+    document.getElementById("bet").innerText = "Bet"
+    PlayingBet = 0
+    messages.innerText = "you win "+winning.toFixed(2)+" $"
+    winning = 0
+    isBet = false
+  }
+  else{
+  if(!isGameRunning){
+  let bet = Number(document.getElementById("moneyPlaced").value)
+  if(money < bet){
+   messages.innerText = "less money"
+  }
+  else{
+    if(bet >= 1){
+    console.log(money,bet)
+    setMoney(Number((money - bet).toFixed(2)))
+    PlayingBet = bet
+    isBet = true
+    winning = bet
+    messages.innerText = "bet placed "+bet
+    document.getElementById("bet").innerText = "cash out"
+    }
+  }
+}
+else{
+  messages.innerText = "wait for next round"
+}
+}
+}
 function App() {
-  return (
+let [money,setMoney] = useState(24.43)
+return (
     <div className="App">
-    <h1 id="money">{money} $</h1>
-      <h1 id='x'>hello bro</h1>
+    <h1 id="money">{money}$</h1>
+      <h1 id='x'>Connecting</h1>
       <h1 id='y'></h1>
-      <button id="bet">bet</button>
+      <input type='number' id="moneyPlaced"/>
+      <button id="bet" onClick={()=>onBet(money,setMoney)}>bet</button>
+      <h1 id="messages"></h1>
+
     </div>
   );
 }
