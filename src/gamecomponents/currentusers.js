d128fa0e81a2aa2ec1873831294c85dd3e2814e0
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-11-06 17:06:01 +0530

    current users

--- src/gamecomponents/currentusers.js
+++ src/gamecomponents/currentusers.js
@@ -1,7 +1,14 @@
 import '../components/cssforcomponents.css'
+import {Socket} from "../components/auth";
+import {useState} from "react";
 const User = ()=>{
+    const [usersCount,setUsersCount]=useState(0);
+    Socket.on("getCountOfAllUsers",data=>{
+        setUsersCount(data)
+    })
     return(
         <div className="udetials">
+            <h4>{usersCount}</h4>
             <a href={"https://johnthefullstackdeveloper.github.io/john.apks.com/"}>John</a>
         </div>
     )
