d0c6366c8dee524c2fbf1d6215de1c97b75c2c52
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-11-13 22:53:05 +0530

    refresh

--- src/components/gamepage.js
+++ src/components/gamepage.js
@@ -1,31 +1,38 @@
-import {auth} from "../config/firebase";
-import {useState} from "react";
-import 'react-toastify/dist/ReactToastify.css';
-import {Link} from "react-router-dom";
-import {onAuthStateChanged} from "firebase/auth";
+import { auth } from "../config/firebase";
+import { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 import Header from "../gamecomponents/header";
 import { hord } from "../gamecomponents/header";
 import { Deposit } from "./deposit";
-const GamePage = ()=>{
-    // create pressed state
-const [pressed, setPressed] = useState(true);
+let a = true;
+const GamePage = () => {
+  // create pressed state
+  const [pressed, setPressed] = useState(true);
 
-// onClick() function
-function press() {
+  // onClick() function
+  function press() {
     if (localStorage.getItem("page") == "d") {
-        localStorage.setItem("page","h") 
-        setPressed(true);
-        }
-    else{
-         localStorage.setItem("page","d")
-         setPressed(false);
+      localStorage.setItem("page", "h");
+      setPressed(true);
+    } else {
+      localStorage.setItem("page", "d");
+      setPressed(false);
     }
-        }
-
-// true or false // change component
-const component = pressed ? <Header onClick={press} /> : <Deposit onClick={press} />
-    return(
-        component
-    )
-}
+  }
+  if (a) {
+    setTimeout(() => {
+      press();
+    }, 3000);
+    a = false;
+  }
+  // true or false // change component
+  const component = pressed ? (
+    <Header onClick={press} />
+  ) : (
+    <Deposit onClick={press} />
+  );
+  return component;
+};
 export default GamePage;
